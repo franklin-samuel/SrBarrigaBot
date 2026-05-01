886ad5ca960e617a3a4928deb41d03ce197db985
Samuel Franklin <samuelfranklinff@gmail.com>
2026-05-01 20:58:34 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -26,6 +26,11 @@ async function bootstrap() {
         forbidNonWhitelisted: true,
         transform: true,
   }));
+  app.enableCors({
+        origin: 'http://localhost:3000',
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
